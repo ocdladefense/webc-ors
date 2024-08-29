b7bd80e21ba25eb4411a996c19c4e2682b4fbc37
José Bernal <jbernal.web.dev@gmail.com>
2024-08-28 18:46:48 -0700

    Simplify the component.

--- src/WebcOrs.js
+++ src/WebcOrs.js
@@ -1,39 +1,37 @@
 import HttpClient from "@ocdla/lib-http/HttpClient.js";
 import Url from "@ocdla/lib-http/Url.js";
-import OrsChapter from "@ocdladefense/ors/src/OrsChapter.js";
-import {OrsParser} from "@ocdladefense/ors/src/OrsParser.js";
-
+import OrsChapter from "@ocdladefense/ors/src/Chapter.js";
+import './mycss.css';
 
 
 const ORS_ENDPOINT = "https://appdev.ocdla.org/books-online/index.php";
 
 
 
-export default class WebcOrs extends HTMLElement {
+export default class WebcOrs extends HTMLDivElement {
+  references;
 
-    references
+  // The ORS chapter to display.
+  chapterNumber;
 
-    // The ORS chapter to display.
-    chapterNumber;
+  // The chapter section to display.
+  sectionNumber = null;
 
-    // The chapter section to display.
-    sectionNumber = null;
+  // An array consisting of 0 or more subsections to display.
+  subsections = null;
 
-    // An array consisting of 0 or more subsections to display.
-    subsections = null;
+  chapter = null;
 
-    chapter = null;
+  constructor() {
+    super();
+    let refs =
+      this.getAttribute("references") &&
+      this.getAttribute("references").split(" ")[1];
+    this.references = refs.split(",").map((ref) => ref.trim());
+    this.chapterNumber = "127"; //this.getAttribute("chapter");
+    this.sectionNumber = "45"; //this.getAttribute("section");
 
-
-
-
-    constructor() {
-        super();
-        let refs = this.getAttribute("references") && this.getAttribute("references").split(" ")[1];
-        this.references = refs.split(",").map((ref) => ref.trim());
-        this.chapterNumber = this.getAttribute("chapter");
-        this.sectionNumber = this.getAttribute("section");
-
+    /*
         // console.log(this.references);
         if (null != this.references) {
             [this.chapterNumber, this.sectionNumber] = this.references[0].split(/\.|\(/);
@@ -41,88 +39,87 @@ export default class WebcOrs extends HTMLElement {
         } else {
             this.references = [[this.chapterNumber, this.sectionNumber].join(".")];
         }
+        */
+    // console.log(this.chapterNumber, this.sectionNumber);
+  }
 
-        // console.log(this.chapterNumber, this.sectionNumber);
+  // Called each time the element is appended to the window/another element.
+  async connectedCallback() {
+    let nodes = [];
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    let styles = document.createElement("style");
+    styles.innerText = WebcOrs.getCss();
+
+    try {
+        nodes = await this.doSomething();
+    } catch (e) {
+        console.error(e);
+        nodes.push(document.createTextNode("An error occurred: " + e.message));
+    }
+    // this.shadowRoot.appendChild(styles);
+
+    this.shadowRoot.append(...nodes);
+  }
+
+
+
+  async doSomething() {
+    let refHtml = [];
+    let error = null;
+
+    // Thee elements will be appended to the shodow DOM.
+    let statute, label, styles;
+
+    // We will display the statute text from the Oregon Legislature website;
+    // and the label will be the ORS citation.
+    // Add some styling as well.
+    statute = document.createElement("div");
+    label = document.createElement("span");
+    
+    
+    statute.setAttribute("class", "statute");
+    label.setAttribute("class", "section-label");
+
+    // For multiple references, this should iterate to create separate labels and statutes.
+    label.appendChild(document.createTextNode(this.references[0]));
+    
+
+
+    let chapter = await WebcOrs.loadChapter(this.chapterNumber);
+    let selector = "#section-" + this.sectionNumber;
+
+    // Currently there is an issue because our source document has all kinds of nasty <html> tags in it.
+    let sections = [chapter.doc.querySelector(selector)];
+    
+    if (null == sections) {
+      throw new Error(
+        "Could not retrieve section for " + this.references.join("\n")
+      );
     }
 
+    
+    statute.prepend("I am appending some content here...");
+    return [label, statute];//statute.append(...sections)];
+  }
 
-    static async loadChapter(chapterNumber) {
+  static async loadChapter(chapterNumber) {
+    const url = new Url("https://appdev.ocdla.org/books-online/index.php");
 
-        const url = new Url("https://appdev.ocdla.org/books-online/index.php");
-        url.buildQuery("chapter", chapterNumber.toString());
+    url.buildQuery("chapter", chapterNumber.toString());
 
-        const req = new Request(url.toString());
-        console.log(req);
-        const client = new HttpClient();
-        const resp = await client.send(req);
-        console.log(resp);
-        console.log("loading chapter");
-        const msword = OrsChapter.fromResponse(resp.clone());
-        msword.chapterNum = chapterNumber;
-
-        // console.log(msword);
-
-        return OrsChapter.toStructuredChapter(msword);
-    }
+    const client = new HttpClient();
+    const req = new Request(url.toString());
+    const resp = await client.send(req);
+    const msword = await OrsChapter.fromResponse(resp);
+    msword.chapterNum = chapterNumber;
+    return OrsChapter.toStructuredChapter(msword);
+  }
 
 
-
-    // Called each time the element is appended to the window/another element.
-    async connectedCallback() {
-        const shadow = this.attachShadow({ mode: "open" });
-
-        const list = document.createElement("div");
-        list.setAttribute("class", "statute");
-        const style = document.createElement("style");
-        style.innerText = WebcOrs.getCss();
-
-        const serializer = new XMLSerializer();
-
-        this.chapter = await WebcOrs.loadChapter(this.chapterNumber);
-        
-
-        console.log(this.references);
-        
-
-        let refHtml = [];
-        let error = null;
-
-        try {
-            let sections = this.chapter.querySelectorAll(this.references);
-            if(null == sections) {
-                throw new Error("Could not retrieve section for "+this.references.join("\n"));
-            }
-            let htmlArray = (!sections || sections.length == 0) ? "Reference not found!" : [...sections].map((section) => serializer.serializeToString(section));
-
-            console.log(htmlArray);
-
-            for (var i = 0; i < htmlArray.length; i++) {
-                let html = OrsParser.replaceAll(htmlArray[i]);
-                refHtml.push(`<span class="section-label">${this.references[i]}</span>` + this.render(html));
-            }
-        } catch(e) {
-            error = e.message;
-            console.error(e);
-        }
-        
-        console.log(this);
-        list.innerHTML = null != error ? error : refHtml.join("\n");
-
-        this.shadowRoot.append(style, list);
-    }
-
-
-
-
-
-    render(data) {
-        return `<div>
-            <p>${data}</p>
-        </div>`;
-    }
-
-    static getCss() {
-        return `
+  static getCss() {
+    return `
         .subsection {
             display:inline-block;
             margin-top: 8px;
@@ -166,10 +163,6 @@ export default class WebcOrs extends HTMLElement {
             font-weight: bold;
         }
         `;
-    }
-
-
-
-
+  }
 }
 
